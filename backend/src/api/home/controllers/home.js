103dc0830e448205765cc73b327c47bc98ec0fc3
Juan Hamilton <hedwards507@gmail.com>
2022-07-03 22:50:18 -0500

    Populate API media and component attribues

--- backend/src/api/home/controllers/home.js
+++ backend/src/api/home/controllers/home.js
@@ -1,9 +1,57 @@
-'use strict';
+"use strict";
 
 /**
  *  home controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController("api::home.home", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.findMany("api::home.home", {
+      ...query,
+      populate: {
+        seo: {
+          populate: {
+            shareImage: true,
+          },
+        },
+        nav: {
+          populate: {
+            logo: true,
+            links: true,
+          },
+        },
+        highlights: {
+          populate: {
+            cta: true,
+            imageMobile: true,
+            imageDesktop: true,
+          },
+        },
+        galleryMobile: true,
+        galleryDesktop: true,
+        clientTestimonials: {
+          populate: {
+            testimonies: {
+              populate: {
+                image: true,
+              },
+            },
+          },
+        },
+        footer: {
+          populate: {
+            logo: true,
+            links: true,
+          },
+        },
+      },
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
