f989c1a4c47ddfb453cc7d969ea371e40633b5d2
Juan Hamilton <hedwards507@gmail.com>
2022-07-03 22:54:45 -0500

    Populate API component for Global

--- backend/src/api/global/controllers/global.js
+++ backend/src/api/global/controllers/global.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  *  global controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::global.global');
+module.exports = createCoreController("api::global.global", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.findMany("api::global.global", {
+      ...query,
+      populate: {
+        defaultSeo: true,
+      },
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
